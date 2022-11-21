e00bb653bd021bc3eb01afd8cd7b6dc92ba2ccb5
Vinoothna99 <vinoothna.gangadasu@gmail.com>
2022-11-20 21:58:43 -0500

    first-commit

---
+++ controllers/users/users.js
@@ -0,0 +1,6 @@
+export default [
+    { "username": "alice",   "type": "FACULTY", "id": "123" },
+    { "username": "bob",     "type": "STUDENT", "id": "234" },
+    { "username": "charlie", "type": "FACULTY", "id": "345" }
+]
+
